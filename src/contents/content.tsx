a15f5f183ce44a2cb8e065afd856b3166251d51b
Jake Correa <jcorrea257@gmail.com>
2023-02-16 11:40:40 -0800

    feat: simple hide/show

--- src/contents/content.tsx
+++ src/contents/content.tsx
@@ -13,22 +13,27 @@ export const getStyle = () => {
 
 const PlasmoOverlay = () => {
   const [val, setVal] = useState('')
+  const [show, setShow] = useState(true)
 
   return (
     <div className="fixed top-4 right-4 rounded-md bg-black/50 p-1 font-sans text-gray-900 shadow backdrop-blur-lg">
-      <div className="h-[420px] w-[380px] overflow-y-auto overflow-x-clip rounded-md  bg-white p-3">
-        <div className="text-2xl font-bold">Farseer AI</div>
+      {show ? (
+        <div className="h-[420px] w-[380px] overflow-y-auto overflow-x-clip rounded-md  bg-white p-3">
+          <button onClick={() => setShow(false)}>Hide Farseer</button>
 
-        <div className="mt-5 grid w-full gap-1.5">
-          <Label htmlFor="columnNames">Column Names</Label>
-          <Textarea
-            placeholder="col1, col2, col3"
-            id="columnNames"
-            value={val}
-            onChange={(e) => setVal(e.currentTarget.value)}
-          />
+          <div className="mt-5 grid w-full gap-1.5">
+            <Label htmlFor="columnNames">Column Names</Label>
+            <Textarea
+              placeholder="col1, col2, col3"
+              id="columnNames"
+              value={val}
+              onChange={(e) => setVal(e.currentTarget.value)}
+            />
+          </div>
         </div>
-      </div>
+      ) : (
+        <button onClick={() => setShow(true)}>Open Farseer</button>
+      )}
     </div>
   )
 }
