2a3af89e3e21e34c9a7e54e18de09b9c18796216
Justin Hilliard <justin.a.hilliard@gmail.com>
2023-02-16 22:19:51 -0800

    parser finally looking better

--- src/lib/parser.ts
+++ src/lib/parser.ts
@@ -1,29 +1,31 @@
 import { readFileSync } from 'fs'
 import path from 'path'
 
-import { HTMLElement, Node, NodeType, parse } from 'node-html-parser'
+import { HTMLElement, NodeType, parse } from 'node-html-parser'
 
+// https://platform.openai.com/playground/p/Z8oqEmAXGhdlDzk6ywQmbZSl?model=text-davinci-003
 async function main() {
   const schemaPath = path.join(process.cwd(), 'assets/example.html')
   const htmlPage = readFileSync(schemaPath, 'utf8')
-  const parsed = parse(htmlPage)
+  const parsedDOM = parse(htmlPage)
 
-  const traverse = (element: Node, depth: number) => {
+  const traverse = (element: HTMLElement, depth: number) => {
     if (depth >= 100) {
       return
     }
-    if (element.nodeType === NodeType.TEXT_NODE) {
-      console.log((element as HTMLElement).rawTagName || '')
-      console.log(element.text.trim())
-    }
-
-    element.childNodes.forEach((child) => traverse(child, depth + 1))
+    element.childNodes.forEach((child) => {
+      if (child.nodeType === NodeType.TEXT_NODE && child.text.trim() !== '') {
+        if (!['SCRIPT', 'BODY', 'HTML', 'STYLE', 'NOSCRIPT'].includes(child.parentNode.tagName)) {
+          // console.log('TAG: ', child.parentNode.tagName)
+          // console.log('CONTENT: ', child.textContent.trim())
+        }
+      } else if (child.nodeType === NodeType.ELEMENT_NODE) {
+        traverse(child as HTMLElement, depth + 1)
+      }
+    })
   }
 
-  traverse(parsed, 0)
-
-  console.log(parsed.structuredText)
-  // traverse(parsed, '', 0)
+  traverse(parsedDOM, 0)
 }
 
 main()
