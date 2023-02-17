1b3e50df0e3c1af334e680663e082bb1ed37faed
Justin Hilliard <justin.a.hilliard@gmail.com>
2023-02-16 16:31:07 -0800

    parser-mvp

---
+++ src/lib/parser.ts
@@ -0,0 +1,29 @@
+import { readFileSync } from 'fs'
+import path from 'path'
+
+import { HTMLElement, Node, NodeType, parse } from 'node-html-parser'
+
+async function main() {
+  const schemaPath = path.join(process.cwd(), 'packages/parser/example.html')
+  const htmlPage = readFileSync(schemaPath, 'utf8')
+  const parsed = parse(htmlPage)
+
+  const traverse = (element: Node, depth: number) => {
+    if (depth >= 100) {
+      return
+    }
+    if (element.nodeType === NodeType.TEXT_NODE) {
+      console.log((element as HTMLElement).rawTagName || '')
+      console.log(element.text.trim())
+    }
+
+    element.childNodes.forEach((child) => traverse(child, depth + 1))
+  }
+
+  traverse(parsed, 0)
+
+  console.log(parsed.structuredText)
+  // traverse(parsed, '', 0)
+}
+
+main()
