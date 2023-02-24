59af432fbd83482c67a1ca7ae1835cf233dc1e01
Justin Hilliard <justin.a.hilliard@gmail.com>
2023-02-24 15:56:04 -0800

    refactored to type injection

--- src/lib/outlets/csv.ts
+++ src/lib/outlets/csv.ts
@@ -2,14 +2,7 @@ import type { ICsvOutletConfig, IParsedResult } from '~schemas'
 
 import { OutletBase } from './base'
 
-export class CsvOutlet extends OutletBase {
-  private cfg: ICsvOutletConfig
-
-  constructor(cfg: ICsvOutletConfig) {
-    super()
-    this.cfg = cfg
-  }
-
+export class CsvOutlet extends OutletBase<ICsvOutletConfig> {
   async _send(_payload: IParsedResult): Promise<any> {
     throw new Error('Not implemented')
   }
