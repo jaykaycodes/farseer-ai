7772ad050c81ca4de71c55daf024124f543a3d9a
Justin Hilliard <justin.a.hilliard@gmail.com>
2023-02-25 13:17:35 -0800

    generate fix

--- src/background/messages/generate.ts
+++ src/background/messages/generate.ts
@@ -37,7 +37,7 @@ const handler: PlasmoMessaging.MessageHandler<IGenerateRequest, IGenerateRespons
       model: 'text-davinci-003',
       prompt,
       temperature: 0,
-      max_tokens: 500,
+      max_tokens: 800,
       top_p: 1,
       frequency_penalty: 0,
       presence_penalty: 0,
@@ -45,7 +45,7 @@ const handler: PlasmoMessaging.MessageHandler<IGenerateRequest, IGenerateRespons
       stop: '}',
     })
 
-    const result = `${outputPrefix}${response.completion}`
+    const result = `${outputPrefix}${response.completion} }`
     storage.set('output', result)
     res.send({
       ok: true,
